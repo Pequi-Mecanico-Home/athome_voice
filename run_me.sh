d7a68edf4727eb352a7294118956f50bcaf7629f
Lucas Gris <gris@alunos.utfpr.edu.br>
2022-11-21 11:16:30 -0300

    Remove unecessary files

--- run_me.sh
+++
@@ -1,8 +0,0 @@
-mkdir src launch srv msg
-rsync -r --existing ../jetson-voice-torch/ros/src/* src/
-# rsync -r --existing ../jetson-voice-torch/ros/launch/* launch/
-# rsync -r --existing ../jetson-voice-torch/ros/launch/* launch/
-rsync -r --existing ../jetson-voice-torch/ros/srv/* srv/
-rsync -r --existing ../jetson-voice-torch/ros/msg/* msg/
-# rsync -r --existing ../wake_word/src/* src/
-# rsync -r --existing ../wake_word/launch/* launch/
