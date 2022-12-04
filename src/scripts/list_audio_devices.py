9eed140cdd1c5dd7ad28254d29c4bc6e9688f3ac
Lucas Gris <gris@alunos.utfpr.edu.br>
2022-12-04 15:36:03 -0300

    Add script to list audio devices

---
+++ src/scripts/list_audio_devices.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import pyaudio as pa
+
+#
+# device enumeration
+# 
+_audio_device_info = None
+
+def _get_audio_devices(audio_interface=None):
+    global _audio_device_info
+    
+    if _audio_device_info:
+        return _audio_device_info
+        
+    if audio_interface:
+        interface = audio_interface
+    else:
+        interface = pa.PyAudio()
+        
+    info = interface.get_host_api_info_by_index(0)
+    numDevices = info.get('deviceCount')
+    
+    _audio_device_info = []
+    
+    for i in range(0, numDevices):
+        _audio_device_info.append(interface.get_device_info_by_host_api_device_index(0, i))
+    
+    if not audio_interface:
+        interface.terminate()
+        
+    return _audio_device_info
+
+def list_audio_inputs():
+    """
+    Print out information about present audio input devices.
+    """
+    devices = _get_audio_devices()
+
+    print('')
+    print('----------------------------------------------------')
+    print(f" Audio Input Devices")
+    print('----------------------------------------------------')
+        
+    for i, dev_info in enumerate(devices):    
+        if (dev_info.get('maxInputChannels')) > 0:
+            print("Input Device ID {:d} - '{:s}' (inputs={:.0f}) (sample_rate={:.0f})".format(i,
+                  dev_info.get('name'), dev_info.get('maxInputChannels'), dev_info.get('defaultSampleRate')))
+                 
+    print('')
+    
+    
+def list_audio_outputs():
+    """
+    Print out information about present audio output devices.
+    """
+    devices = _get_audio_devices()
+    
+    print('')
+    print('----------------------------------------------------')
+    print(f" Audio Output Devices")
+    print('----------------------------------------------------')
+        
+    for i, dev_info in enumerate(devices):  
+        if (dev_info.get('maxOutputChannels')) > 0:
+            print("Output Device ID {:d} - '{:s}' (outputs={:.0f}) (sample_rate={:.0f})".format(i,
+                  dev_info.get('name'), dev_info.get('maxOutputChannels'), dev_info.get('defaultSampleRate')))
+                  
+    print('')
+
+def list_audio_devices():
+    """
+    Print out information about present audio input and output devices.
+    """
+    list_audio_inputs()
+    list_audio_outputs()
+
+if __name__ == "__main__":
+    list_audio_devices()
+   
+    
